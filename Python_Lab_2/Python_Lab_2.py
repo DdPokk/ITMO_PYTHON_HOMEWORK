b44dcd0599af87ac0a836e82e075b3721fe9ae69
DdPokk <9111322054@mail.ru>
2022-06-07 11:53:01 +0300

    обновлено

--- Python_Lab_2/Python_Lab_2.py
+++ Python_Lab_2/Python_Lab_2.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # lab 2
 
 string1 = "   this is a string.  "
@@ -70,3 +71,64 @@ a = 2/3
 b = 2/9
 print("{:7.3f} {:7.3f}".format(a, b))
 print("{:10.3e} {:10.3e}".format(a, b))
+
+print("---List---")
+list1 = [82, 8, 23, 97, 92, 44, 17, 39, 11, 12]
+print(dir(list))
+help(list.insert)
+help(list.append)
+help(list.sort)
+help(list.remove)
+help(list.reverse)
+
+list1[4] = 15
+list1.append(22)
+list1.insert(5,6)
+list1.pop(len(list1)-1)
+print(list1)
+
+print("---SortList___")
+list1.sort(reverse=True)
+print(list1)
+
+list2 = [3,5,6,2,33,6,11]
+lis = sorted(list2)
+print(list2)
+print(lis)
+
+print("---Tuple---")
+print(dir(tuple))
+help(tuple.index)
+help(tuple.count)
+
+seq = (2,8,23,97,92,44,17,39,11,12)
+print(seq.count(8))
+print(seq.index(44))
+listseq = list(seq)
+print(type(listseq))
+print(listseq)
+listseq.sort()
+listseq.append(17)
+print(listseq)
+
+print("---Dictionary---")
+D = {'food': 'Apple', 'quantity': 4, 'color': 'Red'}
+print(D['food'])
+D['quantity'] += 10
+print(D['quantity'])
+
+dp = {}
+dp['name'] = input('Name:')
+dp['age'] = input('Age:')
+print(dp)
+
+print("---Data---")
+rec = {'name': {'firstname': 'Bob', 'lastname': 'Smith'},
+ 'job': ['dev', 'mgr'],
+ 'age': 25}
+
+print(rec['name']['firstname'], ' ',rec['name']['lastname'])
+print(rec['name']['firstname'])
+print(rec['job'])
+rec['job'].append('janitor')
+print(rec)
