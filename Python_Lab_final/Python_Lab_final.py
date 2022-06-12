790d005cfa953239cdfc3a570d08ab2ddf4b8a5c
DmDm <9111322054@mail.ru>
2022-06-12 10:16:23 +0300

    обновлено

--- Python_Lab_final/Python_Lab_final.py
+++ Python_Lab_final/Python_Lab_final.py
@@ -1,5 +1,16 @@
 # Алгоритм 5. Вычисление минимакса
 
+Finish = 99999
+Max = -9999
+Min = 9999
 
+T = int(input('T = '))
+while T != Finish:
+    if T > Max:
+        Max = T
+    if T < Min:
+        Min = T
+    T = int(input('T = '))
+print('Max =', Max, 'Min =', Min)
 
 
